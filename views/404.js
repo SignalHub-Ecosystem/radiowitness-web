102f9b7d6b48f1a1113674fdca1d029bf499c70a
rhodey <notrhodey@protonmail.com>
2019-03-18 16:19:39 -1000

    style

--- views/404.js
+++ views/404.js
@@ -1,15 +1,16 @@
-var html = require('choo/html')
-
-var TITLE = 'radiowitness - route not found'
-
+const html = require('choo/html')
+const TITLE = 'RadioWitness - 404'
 module.exports = view
 
 function view (state, emit) {
-  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  if (state.title !== TITLE) {
+    emit(state.events.DOMTITLECHANGE, TITLE)
+  }
+
   return html`
     <body class="sans-serif pa3">
-      <h1>Route not found.</h1>
-      <a class="pt2" href="/">Back to main.</a>
+      <h1>404</h1>
+      <a class="pt2" href="/">home</a>
     </body>
   `
 }
