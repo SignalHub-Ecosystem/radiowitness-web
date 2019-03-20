9ef2109bbdf7b8a03e15056c9e890c67093a9d81
rhodey <notrhodey@protonmail.com>
2019-03-19 14:31:52 -1000

    working signalhub demo.

--- stores/root.js
+++ stores/root.js
@@ -1,16 +1,47 @@
+const hypercore = require('hypercore')
+const ram       = require('random-access-memory')
+const swarms    = require('@geut/discovery-swarm-webrtc')
+const signalhub = require('signalhub')
+const dat       = require('../dat.json')
+
 module.exports = store
 
 function store (state, emitter) {
   state.readme = 'loading...'
 
-  emitter.on('DOMContentLoaded', function () {
-    fetch(new Request('assets/README.md', { cache : 'reload' }))
-      .then((r) => r.text())
-      .then((readme) => emitter.emit('doc:readme', readme))
+  emitter.on('error', (err) => {
+    console.error('!!! error -> ', err)
+  })
 
-    emitter.on('doc:readme', function (readme) {
-      state.readme = readme
-      emitter.emit(state.events.RENDER)
+  emitter.on('doc:readme', (readme) => {
+    state.readme = readme
+    emitter.emit(state.events.RENDER)
+  })
+
+  emitter.on('dat:ready', (core) => {
+    console.log('readyyy')
+    state.core = core
+
+    let hub = signalhub('app00', ['https://rhodey.org:9001'])
+    hub.subscribe('chan00').on('data', (msg) => {
+      console.log('message -> ', msg)
     })
+
+    hub.broadcast('chan00', { hello : 'world' })
+
+    emitter.emit(state.events.RENDER)
+  })
+
+  emitter.on('DOMContentLoaded', () => {
+    /*fetch(new Request('assets/README.md', { cache : 'reload' }))
+      .then((r) => r.text())
+      .then((readme) => emitter.emit('doc:readme', readme))*/
+
+    let opts = { sparse : true }
+    let key = dat.links.publisher[0].href.split('dat://')[1]
+    let core = hypercore((fname) => ram(), key, opts)
+
+    core.once('error', (err) => emitter.emit('error', err))
+    core.once('ready', () => emitter.emit('dat:ready', core))
   })
 }
--- views/main.js
+++ views/main.js
@@ -2,6 +2,7 @@ const html = require('choo/html')
 const dat  = require('../dat.json')
 
 const TITLE = 'RadioWitness'
+let key = dat.links.publisher[0].href.split('dat://')[1]
 module.exports = view
 
 function view (state, emit) {
@@ -9,8 +10,12 @@ function view (state, emit) {
     emit(state.events.DOMTITLECHANGE, TITLE)
   }
 
+  console.log(dat.links.publisher[0])
+  console.log('!!! key', key)
+
   return html`<body>
     <h2>${dat.title}</h2>
     <p>${dat.description}</p>
+    <p>author.key -> ${key}</p>
   </body>`
 }
