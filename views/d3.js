705ea5445299495f939bc0182df78326870ef0b6
rhodey <notrhodey@protonmail.com>
2019-08-18 11:46:59 -0400

    start from more simple graph

--- views/d3.js
+++ views/d3.js
@@ -5,35 +5,6 @@ const dat  = require('../dat.json')
 
 module.exports = view
 
-function drag (simulation) {
-  function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-
-  function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-
-  return d3.drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended);
-}
-
-function color (d) {
-  if (d.group == "Cited Works") return "blue"
-  else return "orange"
-}
-
 class Graph extends Comp {
   constructor (id, state, emit) {
     super(id)
@@ -45,59 +16,55 @@ class Graph extends Comp {
 
   load (elem) {
     console.log('!!! load()')
-    let data = d3.json("https://gist.githubusercontent.com/mbostock/74cb803c013404ac30e63f020a52a2fd/raw/c7c74c939b602c56c80848963f9ad24802baaead/graph.json")
-    data.then(this.next)
+    let data = d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json")
+    data.then(this.graph.bind(this))
   }
 
-  next(data) {
-    let height = 800
-    let width = 1024
-    const links = data.links.map(d => Object.create(d));
-    const nodes = data.nodes.map(d => Object.create(d));
 
-    const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+  graph(data) {
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+      width = 400 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
 
-    const svg = d3.select(".chart").append("svg")
-      .attr("viewBox", [-width / 2, -height / 2, width, height]);
+    var svg = d3.select(".chart")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+    var link = svg
+      .selectAll("line")
+      .data(data.links)
+      .enter()
+      .append("line")
+        .style("stroke", "#aaa")
 
-    const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-      .attr("r", 5)
-      .attr("fill", color)
-      .call(drag(simulation));
+    var node = svg
+      .selectAll("circle")
+      .data(data.nodes)
+      .enter()
+      .append("circle")
+        .attr("r", 20)
+        .style("fill", "#69b3a2")
 
-    node.append("title")
-        .text(d => d.id);
+    var simulation = d3.forceSimulation(data.nodes)
+      .force("link", d3.forceLink()
+        .links(data.links)
+        .id((d) => d.id)
+      ).force("charge", d3.forceManyBody().strength(-400))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", () => {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
 
-    simulation.on("tick", () => {
-      link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
-      node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-    });
-
-    invalidation.then(() => simulation.stop()); 
+        node
+          .attr("cx", (d) => d.x + 6)
+          .attr("cy", (d) => d.y - 6);
+      });
   }
 
   update (posx) {
